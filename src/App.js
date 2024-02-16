e15cc5186f4d9ad8027ea8325e9039a527ea0f6e
Hugo Suárez <hugolsg@ext.inditex.com>
2024-02-16 18:33:17 +0100

    update and delete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Suspense } from 'react';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
 
-const Header=React.lazy(()=>import("Header_remote/Header"))
-const Title2=React.lazy(()=>import("Header_remote/Titles/Title2"))
+const Header = React.lazy(() => import("Header_remote/Header"));
+const Title2 = React.lazy(() => import("Header_remote/Titles/Title2"));
+console.info(`Starting application with process.env.WEBPACK_DEMO_FOO = "${process.env.WEBPACK_DEMO_F003}`);
+const title=process.env.WEBPACK_DEMO_F004
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Suspense >
+        {/* <Suspense>
           <Header />
         </Suspense>
-        <Suspense >
+        <Suspense>
           <Title2 />
-        </Suspense>
-
+        </Suspense> */}
+        {title}
         <h3>ola</h3>
       </header>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin"); 
-const { dependencies } = require("./package.json");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+// const { dependencies } = require("./package.json");
 const path = require("path");
+const { config } = require('dotenv');
+const { DefinePlugin } = require('webpack');
 
+
+config();
 
 module.exports = {
-    entry: "./src/entry.js",
+  entry: "./src/entry.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: 'auto',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "auto",
   },
   mode: "development",
   devServer: {
@@ -19,32 +23,40 @@ module.exports = {
       directory: path.join(__dirname, "src"),
     },
   },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"],
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader", "postcss-loader"],
-          },
-        ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
       },
-   
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        }),
-        new ModuleFederationPlugin({
-          name: "host",  
-          remotes: { 
-              Header_remote: "Header@http://localhost:3000/remoteEntry.js",            
-          },
-         
-      }),
-        
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
-   
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "host",
+      remotes: {
+        Header_remote: "Header@http://localhost:3000/remoteEntry.js",
+      },
+    }),
+
+    new DefinePlugin(
+      Object.keys(process.env)
+        .filter((key) => key.startsWith('WEBPACK_DEMO_'))
+        .reduce(
+          (result, key) => ({
+            ...result,
+            [`process.env.${key}`]: JSON.stringify(process.env[key])
+          }),
+          {}))
+
+  ],
 };
